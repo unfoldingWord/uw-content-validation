2cfae54bb65b37b38f732634b4149f87ddcacfbc
Robert Hunt <Freely.Given.org@gmail.com>
2020-05-20 07:42:19 +1200

    Disable old tests

--- cypress/integration/wordcount-basic.spec.js
+++ cypress/integration/wordcount-basic.spec.js
@@ -7,38 +7,38 @@ describe('Word Count Basic Tests', function () {
   })
 
 
-  it('Should find 2 as frequency', function() {
-    // first click to the WordCountBasic component
-    cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
-    .click()
+  // it('Should find 2 as frequency', function() {
+  //   // first click to the WordCountBasic component
+  //   cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
+  //   .click()
 
-    cy.get('[data-testid=WordCountBasic-example-1] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .MuiPaper-root > [style="overflow-x: auto;"] > :nth-child(1) > [style="overflow-y: auto;"] > .MuiTable-root > .MuiTableBody-root > [index="0"] > [value="2"]')
-    .should('have.text','2')
-  })
+  //   cy.get('[data-testid=WordCountBasic-example-1] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .MuiPaper-root > [style="overflow-x: auto;"] > :nth-child(1) > [style="overflow-y: auto;"] > .MuiTable-root > .MuiTableBody-root > [index="0"] > [value="2"]')
+  //   .should('have.text','2')
+  // })
 
-  it('Should find 58 words in Markdown text', function() {
-    // first click to the WordCountBasic component
-    cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
-    .click()
+  // it('Should find 58 words in Markdown text', function() {
+  //   // first click to the WordCountBasic component
+  //   cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
+  //   .click()
 
-    cy.get('[data-testid=WordCountBasic-example-5] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .WordCountBasic-root-1 > :nth-child(1)')
-    .should('have.text','58')
-  })
+  //   cy.get('[data-testid=WordCountBasic-example-5] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .WordCountBasic-root-1 > :nth-child(1)')
+  //   .should('have.text','58')
+  // })
 
-  it('Should find 35 words in USFM text', function() {
-    // first click to the WordCountBasic component
-    cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
-    .click()
+  // it('Should find 35 words in USFM text', function() {
+  //   // first click to the WordCountBasic component
+  //   cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
+  //   .click()
 
-    cy.get('[data-testid=WordCountBasic-example-7] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .WordCountBasic-root-1 > :nth-child(1)').should('have.text','35')
-  })
+  //   cy.get('[data-testid=WordCountBasic-example-7] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .WordCountBasic-root-1 > :nth-child(1)').should('have.text','35')
+  // })
 
-  it('Should find 565 words in UTN (column 8) text', function() {
-    // first click to the WordCountBasic component
-    cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
-    .click()
+  // it('Should find 565 words in UTN (column 8) text', function() {
+  //   // first click to the WordCountBasic component
+  //   cy.get(':nth-child(2) > .rsg--list-54 > .rsg--item-55 > .rsg--link-25')
+  //   .click()
 
-    cy.get('[data-testid=WordCountBasic-example-9] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .WordCountBasic-root-1 > :nth-child(1)')
-    .should('have.text','565')
-  })
+  //   cy.get('[data-testid=WordCountBasic-example-9] > .rsg--preview-42 > :nth-child(1) > .MuiPaper-elevation1 > .WordCountBasic-root-1 > :nth-child(1)')
+  //   .should('have.text','565')
+  // })
 })
