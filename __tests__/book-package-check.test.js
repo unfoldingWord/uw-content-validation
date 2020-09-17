ae1fe16d4878b1a30108da8d5d427f14e2ace80c
Bruce McLean <mcleanb@photo-nomad.net>
2020-09-17 16:10:41 -0400

    adding tests for checkBookPackage()

--- __tests__/book-package-check.test.js
+++ __tests__/book-package-check.test.js
@@ -12,10 +12,11 @@ const optionalCheckingOptions = {
     const filePath = Path.join('./__tests__/fixtures', username, repository, path);
 
     if (testFiles.hasOwnProperty(filePath)) { // see if we have a test file to use
-      return testFiles[filePath];
-    }
-
-    if (fs.existsSync(filePath)) {
+      if (testFiles[filePath] !== null) {  // if file content not null, then return contents.  Otherwise will throw exception
+        return testFiles[filePath];
+      }
+      throw `Simulated error - Could not find ${filePath}`;
+    } else if (fs.existsSync(filePath)) {
       return fs.readFileSync(filePath).toString();
     }
     throw `Could not find ${filePath}`;
@@ -44,7 +45,7 @@ describe('checkBookPackage()', () => {
     const languageCode = 'en';
     const bookID = 'rut';
     const rawResults = await checkBookPackage(username, languageCode, bookID, () => {}, optionalCheckingOptions);
-    expect(rawResults.noticeList.length).toEqual(14);
+    expect(rawResults.noticeList.length).toEqual(2);
     const filteredResults = {
       successList: rawResults.successList,
       noticeList: rawResults.noticeList,
