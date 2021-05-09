e6553260a7abc8265f41b30a034860b9a5237ab7
Robert Hunt <Freely.Given.org@gmail.com>
2021-05-10 09:58:04 +1200

    Small fixes

--- src/core/tn-tsv9-row-check.js
+++ src/core/tn-tsv9-row-check.js
@@ -421,7 +421,7 @@ export async function checkTN_TSV9DataRow(languageCode, repoCode, line, bookID, 
         }
         else // TODO: Find more details about when these fields are really compulsory (and when they're not, e.g., for 'intro') ???
             if (V !== 'intro' && occurrence !== '0')
-                addNoticePartial({ priority: 919, message: "Missing Quote field", fieldName: 'OrigQuote', rowID, location: ourRowLocation });
+                addNoticePartial({ priority: 919, message: "Missing OrigQuote field", fieldName: 'OrigQuote', rowID, location: ourRowLocation });
 
         if (occurrence.length) { // This should usually be a digit
             if (occurrence === '0') { // zero means that it doesn’t occur
