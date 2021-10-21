3f7479f395547701162063361dbc105b5f9994e5
Robert Hunt <Freely.Given.org@gmail.com>
2021-10-22 10:22:52 +1300

    Update TA/TW and OBS wrapper functions

--- src/core/wrapper.js
+++ src/core/wrapper.js
@@ -10,6 +10,7 @@ import { checkNotesTSV7Table } from './notes-tsv7-table-check';
 import { internalCheckTN_TSV9Table } from './tn-tsv9-table-check';
 import { checkQuestionsTSV7Table } from './questions-tsv7-table-check';
 import { internalCheckTWL_TSV6Table } from './twl-tsv6-table-check';
+import { checkMarkdownFileContents } from './markdown-file-contents-check';
 import { removeDisabledNotices } from './disabled-notices';
 
 // NOTE: We don't need to know the org name or the repo name here
@@ -17,33 +18,32 @@ import { removeDisabledNotices } from './disabled-notices';
 
 export async function checkTN_TSV7Table(username, languageCode, bookID, tableText, checkingOptions) {
   // Note: the filename and givenLocation parameters are left blank
-  let checkResults = await checkNotesTSV7Table(username, languageCode, 'TN', bookID, '', tableText, '', {...checkingOptions, suppressNoticeDisablingFlag: false});
+  let checkResults = await checkNotesTSV7Table(username, languageCode, bookID === 'OBS' ? 'OBS-TN' : 'TN', bookID, '', tableText, '', { ...checkingOptions, suppressNoticeDisablingFlag: false });
+  if (!checkingOptions?.suppressNoticeDisablingFlag) {
+    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
+  }
+  return checkResults;
+}
+export async function checkSN_TSV7Table(username, languageCode, bookID, tableText, checkingOptions) {
+  // Note: the filename and givenLocation parameters are left blank
+  let checkResults = await checkNotesTSV7Table(username, languageCode, bookID === 'OBS' ? 'OBS-SN' : 'SN', bookID, '', tableText, '', { ...checkingOptions, suppressNoticeDisablingFlag: false });
   if (!checkingOptions?.suppressNoticeDisablingFlag) {
     checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
   }
   return checkResults;
 }
 
-export async function checkTN_TSV9Table(username, languageCode, bookID, tableText, checkingOptions) {
-  // Note: the filename and givenLocation parameters are left blank
-  let checkResults = await internalCheckTN_TSV9Table(username, languageCode, 'TN', bookID, '', tableText, '', {...checkingOptions, suppressNoticeDisablingFlag: false});
-  if (!checkingOptions?.suppressNoticeDisablingFlag) {
-    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
-  }
-  return checkResults;
-}
-
-export async function checkSQ_TSV7Table(username, languageCode, bookID, tableText, checkingOptions) {
-  // Note: the filename and givenLocation parameters are left blank
-  let checkResults = await checkQuestionsTSV7Table(username, languageCode, 'SQ', bookID, '', tableText, '', checkingOptions)
-  if (!checkingOptions?.suppressNoticeDisablingFlag) {
-    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
-  }
-  return checkResults;
-}
 export async function checkTQ_TSV7Table(username, languageCode, bookID, tableText, checkingOptions) {
   // Note: the filename and givenLocation parameters are left blank
-  let checkResults = await checkQuestionsTSV7Table(username, languageCode, 'TQ', bookID, '', tableText, '', checkingOptions)
+  let checkResults = await checkQuestionsTSV7Table(username, languageCode, bookID === 'OBS' ? 'OBS-TQ' : 'TQ', bookID, '', tableText, '', checkingOptions)
+  if (!checkingOptions?.suppressNoticeDisablingFlag) {
+    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
+  }
+  return checkResults;
+}
+export async function checkSQ_TSV7Table(username, languageCode, bookID, tableText, checkingOptions) {
+  // Note: the filename and givenLocation parameters are left blank
+  let checkResults = await checkQuestionsTSV7Table(username, languageCode, bookID === 'OBS' ? 'OBS-SQ' : 'SQ', bookID, '', tableText, '', checkingOptions)
   if (!checkingOptions?.suppressNoticeDisablingFlag) {
     checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
   }
@@ -52,7 +52,35 @@ export async function checkTQ_TSV7Table(username, languageCode, bookID, tableTex
 
 export async function checkTWL_TSV6Table(username, languageCode, bookID, tableText, checkingOptions) {
   // Note: the filename and givenLocation parameters are left blank
-  let checkResults = await internalCheckTWL_TSV6Table(username, languageCode, 'TWL', bookID, '', tableText, '', checkingOptions)
+  let checkResults = await internalCheckTWL_TSV6Table(username, languageCode, bookID === 'OBS' ? 'OBS-TWL' : 'TWL', bookID, '', tableText, '', checkingOptions)
+  if (!checkingOptions?.suppressNoticeDisablingFlag) {
+    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
+  }
+  return checkResults;
+}
+
+export async function checkTA_markdownArticle(username, languageCode, articleFilepathInRepo, articleFileContent, checkingOptions) {
+  // Note: the givenLocation parameter is left blank
+  let checkResults = await checkMarkdownFileContents(username, languageCode, 'TA', articleFilepathInRepo, articleFileContent, '', checkingOptions);
+  if (!checkingOptions?.suppressNoticeDisablingFlag) {
+    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
+  }
+  return checkResults;
+}
+
+export async function checkTW_markdownArticle(username, languageCode, articleFilepathInRepo, articleFileContent, checkingOptions) {
+  // Note: the givenLocation parameter is left blank
+  let checkResults = await checkMarkdownFileContents(username, languageCode, 'TW', articleFilepathInRepo, articleFileContent, '', checkingOptions);
+  if (!checkingOptions?.suppressNoticeDisablingFlag) {
+    checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
+  }
+  return checkResults;
+}
+
+// This format is scheduled to be deprecated
+export async function checkTN_TSV9Table(username, languageCode, bookID, tableText, checkingOptions) {
+  // Note: the filename and givenLocation parameters are left blank
+  let checkResults = await internalCheckTN_TSV9Table(username, languageCode, 'TN', bookID, '', tableText, '', { ...checkingOptions, suppressNoticeDisablingFlag: false });
   if (!checkingOptions?.suppressNoticeDisablingFlag) {
     checkResults.noticeList = removeDisabledNotices(checkResults.noticeList);
   }
